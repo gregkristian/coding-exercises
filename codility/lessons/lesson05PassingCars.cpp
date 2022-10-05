e0fa0a39c55373e2ab2004ef3df7d8223dbcfb38
gregkristian <gkristianp@gmail.com>
2022-10-05 11:27:59 +0000

    Add PassingCars from lesson. 100% score

---
+++ codility/lessons/lesson05PassingCars.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem Description
+ *
+ * A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+ * Array A contains only 0s and/or 1s:
+ * 0 represents a car traveling east,
+ * 1 represents a car traveling west.
+ * 
+ * The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to
+ * the east and Q is traveling to the west. For example, consider array A such that:
+ * A[0] = 0     A[3] = 1
+ * A[1] = 1     A[4] = 1
+ * A[2] = 0
+ * We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+ * 
+ * Write a function: int solution(vector<int> &A);
+ * that, given a non-empty array A of N integers, returns the number of pairs of passing cars. The function should return −1
+ * if the number of pairs of passing cars exceeds 1,000,000,000.
+ * 
+ * Write an efficient algorithm for the following assumptions:
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer that can have one of the following values: 0, 1.
+ */
+
+
+// Result: Correctness 100%, Performance 100%. Overall 100%
+int solution(vector<int> &A)
+{
+    // write your code in C++14 (g++ 6.2.0)
+    int eastbound_cars = 0;
+    long long total_pair = 0; // USE LONG FOR THIS! Overflow risk
+    for (auto a : A)
+    {
+        if (a == 0)
+        {
+            eastbound_cars++;
+        }
+        else
+        {
+            total_pair += eastbound_cars;
+        }
+    }
+    total_pair = total_pair > 1000000000 ? -1 : total_pair;
+    return total_pair;
+}
