320b35c18cfb020167a22308432718be0b366ec1
gregkristian <gkristianp@gmail.com>
2023-05-10 07:59:37 +0000

    Commiting leftover files

--- codility/tests/test1.cpp
+++ codility/tests/test1.cpp
@@ -11,13 +11,14 @@ Answer is 3 (3*7=21, and is written in binary; "1 0 1 0 1". Three bits are set t
 #include <bit>
 
 int solution(int A, int B)
-{   
+{
     unsigned int C = A*B;
     return std::popcount(C); //C++ 20
 }
 
 int main (void)
-{  
-    std::cout << solution(3, 7) << std::endl; // Expect 3
+{
+    std::cout << solution(100000000, 100000000) << std::endl; // Expect 3
+    std::cout << solution(20, 31) << std::endl; // Expect 3
     return 0;
 }
